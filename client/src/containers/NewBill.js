a69aa41318720dca6172029736c4ad6e6b3c1ff1
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2024-03-01 08:25:50 +0400

    feat (client) : NewBill File Extension Restrictions
    - Add a list of file extensions to restraint the type of files the user could provide
    - Implement a condition to save the file into the store only if the file extensions match any of the ones allowed
    - Add some stylings to the `<input type="file" />` if the user does not provided the right file extensions

--- client/src/containers/NewBill.js
+++ client/src/containers/NewBill.js
@@ -6,51 +6,66 @@ export default class NewBill {
     this.document = document;
     this.onNavigate = onNavigate;
     this.store = store;
+
     const formNewBill = this.document.querySelector(
       `form[data-testid="form-new-bill"]`
     );
     formNewBill.addEventListener("submit", this.handleSubmit);
-    const file = this.document.querySelector(`input[data-testid="file"]`);
-    file.addEventListener("change", this.handleChangeFile);
+
+    this.fileInput = this.document.querySelector(`input[data-testid="file"]`);
+    this.fileInput.addEventListener("change", this.handleChangeFile);
     this.fileUrl = null;
     this.fileName = null;
     this.billId = null;
+
     new Logout({ document, localStorage, onNavigate });
   }
+
   handleChangeFile = (e) => {
     e.preventDefault();
+
     const file = this.document.querySelector(`input[data-testid="file"]`)
       .files[0];
-    const filePath = e.target.value.split(/\\/g);
-    const fileName = filePath[filePath.length - 1];
-    const formData = new FormData();
-    const email = JSON.parse(localStorage.getItem("user")).email;
-    formData.append("file", file);
-    formData.append("email", email);
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true,
-        },
-      })
-      .then(({ fileUrl, key }) => {
-        console.log(fileUrl);
-        this.billId = key;
-        this.fileUrl = fileUrl;
-        this.fileName = fileName;
-      })
-      .catch((error) => console.error(error));
+    // If file extensions are image ones
+    const validExtensions = ["image/jpeg", "image/png"];
+    if (validExtensions.includes(file.type)) {
+      this.fileInput.classList.add("blue-border");
+
+      const filePath = e.target.value.split(/\\/g);
+      const fileName = filePath[filePath.length - 1];
+
+      const formData = new FormData();
+      const email = JSON.parse(localStorage.getItem("user")).email;
+      formData.append("file", file);
+      formData.append("email", email);
+
+      this.store
+        .bills()
+        .create({
+          data: formData,
+          headers: {
+            noContentType: true,
+          },
+        })
+        .then(({ fileUrl, key }) => {
+          this.billId = key;
+          this.fileUrl = fileUrl;
+          this.fileName = fileName;
+        })
+        .catch((error) => console.error(error));
+    } else {
+      this.fileInput.value = null;
+      this.fileInput.classList.remove("blue-border");
+      this.fileInput.style.border = "2px red solid";
+    }
   };
+
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(
-      'e.target.querySelector(`input[data-testid="datepicker"]`).value',
-      e.target.querySelector(`input[data-testid="datepicker"]`).value
-    );
+
     const email = JSON.parse(localStorage.getItem("user")).email;
+
     const bill = {
       email,
       type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
@@ -69,7 +84,9 @@ export default class NewBill {
       fileName: this.fileName,
       status: "pending",
     };
+
     this.updateBill(bill);
+
     this.onNavigate(ROUTES_PATH["Bills"]);
   };
 
